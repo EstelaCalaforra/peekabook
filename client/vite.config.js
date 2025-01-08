20be55d8c4ac6a1a608ff96e57a82dbb4861a1b4
Estela Calaforra <estelacalaforra@gmail.com>
2025-01-08 12:04:52 +0100

    changes in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     // Developer server config
     historyApiFallback: true
