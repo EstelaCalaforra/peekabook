db389f07ef11af25c14c485a3d9bc77ffa4e29ee
Estela Calaforra <estelacalaforra@gmail.com>
2025-01-08 10:20:40 +0100

    redirecting to index.html when not found route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import * as path from 'path'
 // Routers
 import { bookRouter } from './routes/bookRoutes.js'
 import { userRouter } from './routes/userRoutes.js'
@@ -22,6 +23,11 @@ app.use('/api/books', bookRouter)
 
 // Login and Signup
 app.use('/api/users', userRouter)
+
+// Redirect all not found paths to index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
 
 app.listen(port, function (err) {
   if (err) console.log(err)
